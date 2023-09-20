3fc8825485581af49b0d5b12ea864edbe75bba16
Nguyễn Hải <haibetmen997@gmail.com>
2023-09-20 15:52:27 +0700

    no message

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnIndex = document.querySelector("#indexx");
+const btnCoTich = document.querySelector("#cotich");
+const btnNguNgon = document.querySelector("#ngungon");
+const btnEnglish = document.querySelector("#english");
+
+
+
+function loadHtml(numberr) {
+    btnIndex
+    return 1;
+}
